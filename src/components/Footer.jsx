2810fa12d38b03bd42031fec8d8382fc50492384
kittyKissYou <ubiuka@yandex.ru>
2023-08-31 16:46:17 +0900

    go 31/0//2023 16:46

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -15,8 +15,8 @@ const Footer = () => {
             <h2>Телефоны</h2>
             <div className="contacts__left-numbers">
               <a href="tel:+79994109970">+79994109970</a>
-              <a href="tel:+79994109970">+79148044212</a>
-              <a href="tel:+79994109970">+79141418943</a>
+              <a href="tel:+79148044212">+79148044212</a>
+              <a href="tel:+79141418943">+79141418943</a>
             </div>
           </div>
           <div className="contacts__mid">
